fb80b1b0c08af53cb8116436c1cc8d4723a0ee3e
shankarjatin <shankarjatin1005@gmail.com>
2024-12-17 18:06:36 +0530

    url added

--- utils/constants.js
+++ utils/constants.js
@@ -3,7 +3,7 @@
 // constants.js
 
 const LOCAL_API_URL = "http://localhost:8002";
-const PROD_API_URL = "https://muta-backend-24.onrender.com";
+const PROD_API_URL = "https://secure-blink-jatin.onrender.com";
 
 module.exports = {
    LOCAL_API_URL,
